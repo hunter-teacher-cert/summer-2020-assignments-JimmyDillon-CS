49d903bb6ab778f12bb8cd22f811789eb9b39a35
Jimmy Dillon <dillon.jimmy@gmail.com>
2020-06-17 16:06:41 -0400

    new file

---
+++ pre02/Data.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Date {
+    string day = "Wednesday";
+    int date = 17;
+    string month = "June";
+    int year = 2020;
+    public static void main(String[] args){
+        System.out.println(day + ", "+ month + " " + date + ", " + year);
+    }
+}
