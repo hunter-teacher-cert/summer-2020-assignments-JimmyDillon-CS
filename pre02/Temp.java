39441187bc46f6a6c7a481309f11b37344785597
Jimmy Dillon <dillon.jimmy@gmail.com>
2020-06-18 13:27:16 -0400

    new file

---
+++ pre02/Temp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/*
+Write a program that converts a temperature from Celsius to Fahrenheit. It should (1) prompt the user for input, (2) read a double value from the keyboard, (3) calculate the result, and (4) format the output to one decimal place. For example, it should display "24.0 C = 75.2 F".
+Here is the formula. Be careful not to use integer division!
+*/
+
+public class Temp {
+
+    public static void main(String[] args) {
+      Scanner in = new Scanner (System.in);
+
+      
+    }
+}
