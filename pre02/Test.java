a51d13932b680bdcce0e5fbe46618276b7710513
Jimmy Dillon <dillon.jimmy@gmail.com>
2020-06-18 00:14:24 -0400

    new file

---
+++ pre02/Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Trying out some stuff
+ */
+public class Test {
+
+    public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+
+      int inch = 2;
+      double cm = 5.08;
+
+      printf("%d inches is %f centimeters", inch, cm);
+    }
