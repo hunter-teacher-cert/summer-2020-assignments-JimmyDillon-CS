b4ce60aeebd90d5353bd71679bac2bbf6b6b19ee
Jimmy Dillon <dillon.jimmy@gmail.com>
2020-06-25 00:41:39 -0400

    new file

---
+++ pre08/Craps.java
@@ -0,0 +1,27 @@
+import java.math.*;
+import java.util.*;
+
+public class Craps{
+
+  public static void main(String[] args) {
+    
+  }
+
+  public static int roll (int integer){
+    int dieNum = 0;
+    // Do stuff
+    return dieNum;
+  }
+
+  public static int shoot(int dice, int max){
+    int result = 0;
+    // Do stuff
+    return result;
+  }
+
+  public static String round(){
+    boolean win = true;
+
+    return win;
+  }
+}
