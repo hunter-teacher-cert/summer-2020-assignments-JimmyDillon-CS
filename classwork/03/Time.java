4e4def3b107b266e194c232fccaf77cd6d06420c
Jimmy Dillon <dillon.jimmy@gmail.com>
2020-07-06 11:14:06 -0400

    new file

---
+++ classwork/03/Time.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Time{
+
+  private int hour;
+  private int minute;
+  private double second;
+
+  public Time(){
+    this.hour = 0;
+    this.minute = 0.0;
+    this.second = 0.0;
+  }
+
+  public Time(int hour, double minute, double second){
+    this.second = second;
+    this.minute = minute;
+    this.second = second;
+  }
+}
