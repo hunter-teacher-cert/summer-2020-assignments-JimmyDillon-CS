918bac4d5207847bb19269244029d2ab2790c4ea
Jimmy Dillon <dillon.jimmy@gmail.com>
2020-07-06 11:17:36 -0400

    new file

---
+++ classwork/04/SuperArray.java
@@ -0,0 +1,12 @@
+public class SuperArray {
+    private int[] data;
+    private int numberElements;
+
+    public SuperArray(){
+      this.data = new int[10];
+    }
+
+    public SuperArray(int size){
+      this.data = new int[size];
+    }
+}
