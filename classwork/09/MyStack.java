01d3c3a798113a424fda69ec7a87b91954d23174
repl.it user <replituser@example.com>
2020-07-13 18:06:01 +0000

    skeleton of MyStack

---
+++ classwork/09/MyStack.java
@@ -0,0 +1,48 @@
+import java.utli.*;
+import java.io;
+
+public class MyStack extends Stack{
+  
+/*  
+    Converts a String into a stack with each individual character the stack.
+**/
+
+  public Stack<Character> parserStoC (String s){
+    Stack<Character> charStack = new Stack<Character>;
+    return charStack;
+  }
+
+/*  
+Converts a String with spaces into a stack with each individual word stored as a String in the stack
+**/
+
+  public Stack<String> parserStoS (String s){
+    Stack<String> stringStack = new Stack<String>;
+    return stringStack;
+  }
+
+/*
+    returns String s in reverse order by character or returns String s in reverse order by individual Strings if a space is included in the string.
+**/
+  
+  public static String reverse(Sting s){
+    return s;
+  }
+  
+/*
+    Returns true or false depneding on whether s is a palindrome (can be read the same forward & backward)
+**/
+
+  public static boolean isPalindrome(String s){
+    return true;
+  }
+
+/*
+    Returns true if the parentheses in the expression are matched accurately.
+**/
+
+  public static boolean parenCheck(String s){
+    return true;
+  }
+  
+}
