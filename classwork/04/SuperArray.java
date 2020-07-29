f4c015fd7cf94fdf04c0ab9690efac43160476f2
Jimmy Dillon <dillon.jimmy@gmail.com>
2020-07-29 15:52:21 -0400

    SuperArray with all features including remove, grow, new add

--- classwork/04/SuperArray.java
+++ classwork/04/SuperArray.java
@@ -16,14 +16,40 @@ public class SuperArray {
     * This routine will add the parameter value to the end of the SuperArray (don’t forget to update numberElements). If there is no more room in the array do nothing.
     */
     public void add(int value){
-      if (this.numberElements < this.data.length){
-        this.data[numberElements] = value;
-        this.numberElements++;
+      if (this.isFull()){
+        this.grow();
       }
+      data[numberElements] = value;
+      numberElements++;
     }
 
+/*
+    This is a second version of add it will add an element at location index. If index is past the end of the array, do nothing,. Otherwise, shift down all the elements past index to create room and then insert value into the correct location. Don’t forget to grow the array as in the previous question if necessary.
+*/
+
     public void add(int index, int value){
+      if (data.length <= index){
+        return;
+      }
+      if (this.isFull()){
+        this.grow();
+      }
+      int [] tmpArray = new int[numberElements-index];
+      int tmpIndex = 0;
+      // Add ints after insert to a temp array
+      for (int i = index; i < numberElements; i ++){
+        tmpArray[tmpIndex] = data[i];
+        tmpIndex++;
+      }
+      // insert the value at the index;
       this.data[index] = value;
+      // Add ints from tmpArray back to data array
+      int insertIndex = index + 1;
+      for (int i = 0; i < tmpIndex; i++ ){
+        data[insertIndex] = tmpArray[i];
+        insertIndex++;
+      }
+      numberElements++;
     }
 
     /**
@@ -37,11 +63,20 @@ public class SuperArray {
     * Return True if there are no elemets in the internal array, false otherwise.
     */
 
-    public boolean isEmpty(){
+    public boolean isEmpty (){
       if (numberElements == 0) {
         return true;
       }
       else {
+        return false;
+      }
+    }
+
+    public boolean isFull (){
+      if (numberElements == data.length){
+        return true;
+      }
+      else{
         return false;
       }
     }
@@ -60,4 +95,33 @@ public class SuperArray {
       contents = contents + "]";
       return contents;
     }
+
+/*
+    Removes an int at index.
+*/
+
+    public void remove (int index){
+      if (data.length <= index){
+        return;
+      }
+      if (this.isEmpty()){
+        return;
+      }
+      for (int i = index; i < numberElements ; i++ ) {
+        data[i] = data[i+1];
+      }
+      numberElements--;
+    }
+
+/*
+    Grows the array by 50%
+*/
+    public void grow (){
+      int[] newData = new int[data.length + data.length / 2];
+      for (int i = 0; i < numberElements ; i++) {
+        newData[i] = data[i];
+      }
+      data = newData;
+    }
+
 }
